fcbe06106a1c497ab0bf9535b9f0111d5b98eddf
Linda Puspitasari <114373037+lindapuspitasarii@users.noreply.github.com>
2023-03-20 19:14:21 +0700

    Add files via upload
    Nama : Linda Puspitasari
    NIM : B2A020029
    Tugas Teknik Simulasi Pertemuan 1

---
+++ B2A020029_LINDA PUSPITASARI_TUGAS RNG.R
@@ -0,0 +1,34 @@
+#TUGAS RNG
+#NIM GANJIL
+#-Menggunakan Additive
+#-Menggunakan Bernoulli_2
+#Diketahui : z0=11123, a=35, m=138, c=437, n=100, p=0.65
+
+#ADDITIVE
+Additive_RNG<-function(a,z0,c,m,n) {
+  xi<-matrix(NA,n,3)
+  colnames(xi)<-c("aZ(i-1)+c","Xi","Ui")
+  for (i in 1:n)
+  {
+    xi[i,1]<-(a*z0+c)
+    xi[i,2]<-xi[i,1]%%m
+    xi[i,3]<-xi[i,2]/m
+    z0<-xi[i,2]
+  }
+  hist(xi[,3])
+  View(xi)
+}
+Additive_RNG(35,11123,437,138,100)
+
+
+#BERNOULLI 2
+#Angka n diganti sesuai nilai Xi pada output Additive table
+Bernouli_2<-function(n,p) {
+  i<-n
+  p<-p
+  X<-runif(i)
+  Y<-(X<=p)+0
+  (tabel<-table(Y)/length(Y))
+}
+barplot(tabel,main="Bernoulli")
+Bernouli_2(1000, 0.65)
