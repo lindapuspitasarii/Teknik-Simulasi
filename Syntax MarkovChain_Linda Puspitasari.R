0d50fdef8f62991a9ba1fcfab6ae3f41d0a6d1d1
Linda Puspitasari <114373037+lindapuspitasarii@users.noreply.github.com>
2023-04-11 11:12:22 +0700

    Update Syntax MarkovChain_Linda Puspitasari.R

--- Syntax MarkovChain_Linda Puspitasari.R
+++ Syntax MarkovChain_Linda Puspitasari.R
@@ -8,3 +8,16 @@ mc<-new("markovchain", transitionMatrix =mtrx, name ="Evaluasi Perekrutan Karyaw
 #Menghitung peluang status pada tahun ke-3
 steadyStates(mc)[1]
 
+#ANOTHER EXAMPLE
+
+library(markovchain)
+statesNames=c("Serba","Ada")
+mcA<-new("markovchain", transitionMatrix=matrix(c(0.667,0.333,0.625,0.375),byrow=TRUE,
+                                                nrow=2, dimnames=list(statesNames,statesNames)))
+mcA
+
+#operations with S4 methods
+mcA^2
+steadyStates(mcA)
+absorbingStates(mcA)
+markovchainSequence(n=20, markovchain=mcC, include=TRUE)
