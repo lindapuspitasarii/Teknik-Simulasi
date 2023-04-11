5579989a8b5e243b4601cdf4e05571f179f0952d
Linda Puspitasari <114373037+lindapuspitasarii@users.noreply.github.com>
2023-04-11 10:13:25 +0700

    Add files via upload

---
+++ Syntax MarkovChain_Linda Puspitasari.R
@@ -0,0 +1,10 @@
+library("markovchain")
+
+#Membuat objek markovchain
+mtrx<-matrix(c(0.6, 0.4, 0.2, 0.8), byrow=TRUE, nrow=2,
+             dimnames =list(c("B", "T"),c("B", "T")))
+mc<-new("markovchain", transitionMatrix =mtrx, name ="Evaluasi Perekrutan Karyawan Baru")
+
+#Menghitung peluang status pada tahun ke-3
+steadyStates(mc)[1]
+
